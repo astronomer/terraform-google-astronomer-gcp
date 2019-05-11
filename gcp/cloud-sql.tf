956963f72760c1b62a7ac10085b442210269cc2e
Kaxil Naik <kaxilnaik@gmail.com>
2019-05-11 21:17:57 +0100

    [#21] GCP - Fix Cloud SQL Instance Name error

--- gcp/cloud-sql.tf
+++ gcp/cloud-sql.tf
@@ -1,4 +1,9 @@
+resource "random_id" "db_name_suffix" {
+  byte_length = 4
+}
+
 resource "google_sql_database_instance" "instance" {
+  name             = "astro-db-${random_id.db_name_suffix.hex}"
   region           = "${var.region}"
   database_version = "POSTGRES_9_6"
 
