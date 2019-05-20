e918a4e6178ec57d0046681508901e2c07c48237
Steven Miller <sjmiller609@gmail.com>
2019-05-20 20:10:48 +0000

    added bastion host in the public subnet

---
+++ aws/bastion.tf
@@ -0,0 +1,73 @@
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+provider "http" {
+  version = "1.1"
+}
+
+data "http" "local_ip" {
+  url = "http://ipv4.icanhazip.com/s"
+}
+
+resource "aws_security_group" "bastion_sg" {
+
+  name        = "astronomer_bastion_sg"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    cidr_blocks = ["${trimspace(data.http.local_ip.body)}/32"]
+  }
+
+  egress {
+    # TLS (change to whatever ports you need)
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags          = "${local.tags}"
+}
+
+resource "aws_key_pair" "bastion_ssh_key" {
+  key_name   = "bastion_ssh_key"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
+resource "aws_instance" "bastion" {
+
+  ami           = "${data.aws_ami.ubuntu.id}"
+
+  key_name      = "${aws_key_pair.bastion_ssh_key.key_name}"
+
+  instance_type = "t2.micro"
+
+  subnet_id     = "${module.vpc.public_subnets[0]}"
+
+  vpc_security_group_ids = ["${aws_security_group.bastion_sg.id}"]
+
+  tags          = "${local.tags}"
+
+}
+
+
---
+++ aws/vpc.tf
@@ -0,0 +1,84 @@
+/*
+ Create VPC with public and
+ private subnets
+*/
+
+module "vpc" {
+
+  source = "terraform-aws-modules/vpc/aws"
+  version = "1.14.0"
+
+  name = "${var.base_name}-${var.environment}-vpc"
+
+  cidr = "10.0.0.0/16"
+
+  azs = "${local.azs}"
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets     = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags               = "${local.tags}"
+
+  public_subnet_tags = {
+    "${var.cluster_type == "private" ? "bastion_subnet" : format("%s", "kubernetes.io/cluster/${local.cluster_name}")}" = "${var.cluster_type == "private" ? "1" : "shared"}"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+
+  vpc_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+}
+
+resource "aws_security_group" "worker_group_mgmt_one" {
+  name_prefix = "worker_group_mgmt_one"
+  description = "SG to be applied to all *nix machines"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+    ]
+  }
+}
+
+resource "aws_security_group" "worker_group_mgmt_two" {
+  name_prefix = "worker_group_mgmt_two"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "192.168.0.0/16",
+    ]
+  }
+}
+
+resource "aws_security_group" "all_worker_mgmt" {
+  name_prefix = "all_worker_management"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
+}
