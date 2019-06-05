3b6341f6128c098a7ad62745b4652ede8b41c524
Steven Miller <sjmiller609@gmail.com>
2019-06-05 13:25:46 +0000

    added socks5 proxy through bastion command to output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
+output "bastion_socks5_proxy_command" {
+  value = "gcloud beta compute ssh --zone ${google_compute_instance.bastion.zone} ${google_compute_instance.bastion.name} --tunnel-through-iap --ssh-flag='-D 1234 -C -N'"
+}
+
+output "kubernetes_api_sample_command" {
+  value = "If you have started the api proxy using the bastion SOCKS5 proxy command, this should work:\nhttps_proxy=socks5://127.0.0.1:1234 kubectl get pods"
+}
+
 output "db_connection_string" {
   value = "postgres://${google_sql_user.airflow.name}:${local.postgres_airflow_password}@${google_sql_database_instance.instance.private_ip_address}:5432"
   sensitive = true
@@ -35,7 +43,6 @@ users:
     password: "${google_container_cluster.primary.master_auth.0.password}"
     username: "${google_container_cluster.primary.master_auth.0.username}"
 EOF
-
   sensitive = true
 }
 
