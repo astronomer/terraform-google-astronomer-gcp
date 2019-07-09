64db30f40901053d22a1fa0a0d06c730d8599e44
Steven Miller <sjmiller609@gmail.com>
2019-07-09 14:11:42 +0000

    setting up google credentials

--- pipeline/run_terraform.sh
+++ pipeline/run_terraform.sh
@@ -1,12 +1,18 @@
+#!/bin/bash
+
+echo $GOOGLE_CREDENTIAL_FILE_CONTENT > /tmp/account.json
+
 set -xe
+
+ls /tmp | grep account
+
+export GOOGLE_APPLICATION_CREDENTIALS='/tmp/account.json'
 
 terraform -v
 
-echo $GOOGLE_CREDENTIAL_FILE_CONTENT > ~/account.json
-
 # unique deployment ID to avoid collisions in CI
 # needs to be 32 characters or less and start with letter
-DEPLOYMENT_ID=ci$(echo "$DRONE_REPO_NAME$DRONE_BUILD_NUMBER" | md5sum | awk '{print substr($1,0,30)}')
+DEPLOYMENT_ID=ci$(echo "$DRONE_REPO_NAME$DRONE_BUILD_NUMBER" | md5sum | awk '{print substr($1,0,10)}')
 echo $DEPLOYMENT_ID
 
 cp providers.tf.example examples/$EXAMPLE/providers.tf
