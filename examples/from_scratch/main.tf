944710a9fee9f3605bb35a862c32944ffd9748d3
Steven Miller <sjmiller609@gmail.com>
2019-07-09 13:04:33 +0000

    terraform fmt

--- examples/from_scratch/main.tf
+++ examples/from_scratch/main.tf
@@ -1,9 +1,9 @@
 variable "deployment_id" {}
 
 module "astronomer_gcp" {
-  source = "../.."
-  deployment_id = var.deployment_id
+  source           = "../.."
+  deployment_id    = var.deployment_id
   dns_managed_zone = "steven-zone"
-  project = "astronomer-cloud-dev-236021"
-  admin_emails = ["steven@astronomer.io"]
+  project          = "astronomer-cloud-dev-236021"
+  admin_emails     = ["steven@astronomer.io"]
 }
