6ab5af0931226a03381a70de4f7d776e49f0faca
Steven Miller <sjmiller609@gmail.com>
2019-07-26 12:51:04 +0000

    test update cloud module

--- pipeline/update_cloud_module.sh
+++ pipeline/update_cloud_module.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Install Terraform so we can do 'terraform fmt'
 TERRAFORM_VERSION=0.12.4
@@ -15,7 +15,7 @@ echo "$DEPLOY_KEY" | ssh-add -
 mkdir -p $HOME/.ssh
 ssh-keyscan -t rsa github.com >> $HOME/.ssh/known_hosts
 git clone git@github.com:astronomer/terraform-google-astronomer-cloud.git
-cd google-environments
+cd terraform-google-astronomer-cloud
 sed -i "s/version\s=\s\"[0-9]*\.[0-9]*\.[0-9]*\"/version = \"$DRONE_TAG\"/g" stage/cloud/main.tf
 sed -i "0,/version\s*=\s*\"[0-9]*\.[0-9]*\.[0-9]*\"/ s//version = \"$DRONE_TAG\"/" main.tf
 terraform fmt
