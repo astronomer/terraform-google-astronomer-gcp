4e729d39a7e985dac0631d6d53b2ee68626e3193
sjmiller609 <sjmiller609@gmail.com>
2019-12-09 10:59:56 -0500

    Enable GKE metered billing in CI

--- examples/from_scratch/main.tf
+++ examples/from_scratch/main.tf
@@ -5,10 +5,11 @@ variable "zonal" {
 }
 
 module "astronomer_gcp" {
-  source              = "../.."
-  deployment_id       = var.deployment_id
-  dns_managed_zone    = "steven-zone"
-  email               = "steven@astronomer.io"
-  zonal_cluster       = var.zonal
-  management_endpoint = "public"
+  source                     = "../.."
+  deployment_id              = var.deployment_id
+  dns_managed_zone           = "steven-zone"
+  email                      = "steven@astronomer.io"
+  zonal_cluster              = var.zonal
+  management_endpoint        = "public"
+  enable_gke_metered_billing = true
 }
