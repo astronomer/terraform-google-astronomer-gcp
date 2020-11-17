8601a71b272da4494311829794e11af19321eb79
tommy kelly <tommy@astronomer.io>
2020-11-17 13:56:29 -0500

    better formatting of ternary in ternary :\

--- locals.tf
+++ locals.tf
@@ -40,8 +40,13 @@ users:
 
   EOF
   bastion_name              = "${var.deployment_id}-bastion"
-  # fix bug during destory that leaves the random_string.postgres_airflow_password empty and errors out
-  postgres_airflow_password = var.postgres_airflow_password == "" ? random_string.postgres_airflow_password != [] ? random_string.postgres_airflow_password[0].result : "" : var.postgres_airflow_password
+  # the second ternary is due to a bug during terraform destroy that the random_string.postgres_airflow_password
+  # is an empty array and causes an error.  this just checks and lets it keep going through destroy successfully.
+  postgres_airflow_password = (
+    var.postgres_airflow_password == "" 
+      ? random_string.postgres_airflow_password != [] ? random_string.postgres_airflow_password[0].result : "" 
+      : var.postgres_airflow_password
+  )
   core_network_id = format(
     "projects/%s/global/networks/%s",
     google_compute_network.core.project,
