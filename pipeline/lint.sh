86a4df746dfc5efdcdde08dfeb0a6484ab4ccbfb
pgvishnuram <vishn@astronomer.io>
2021-04-30 13:55:49 +0530

    added prerun terraform script

---
+++ pipeline/lint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -xe
+
+TERRAFORM="${TERRAFORM:-terraform-0.12.31}"
+
+"${TERRAFORM}" -v
+
+cp providers.tf.example providers.tf
+
+"${TERRAFORM}" init
+"${TERRAFORM}" fmt -check=true
+"${TERRAFORM}" validate -var "deployment_id=validate" -var "dns_managed_zone=validate-fake.com" -var "email=fake@mailinator.com"
+
+find examples -maxdepth 1 -mindepth 1 -type d | while read -r example ; do
+  cp providers.tf "${example}"
+  (
+    cd "${example}"
+    echo "${example}"
+    "${TERRAFORM}" init
+    "${TERRAFORM}" fmt -check=true
+    "${TERRAFORM}" validate -var "deployment_id=citest"
+  )
+done
