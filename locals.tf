3ca9f4d2fd80ed27324ff5be1b7e54c491223472
pgvishnuram <vishnu@astronomer.io>
2021-09-29 22:24:00 +0530

    comments added for kubeconfig changes [SKIP CI]

--- locals.tf
+++ locals.tf
@@ -20,7 +20,9 @@ locals {
   cluster_name           = google_container_cluster.primary.name
   endpoint               = google_container_cluster.primary.endpoint
   cluster_ca_certificate = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
-  kubeconfig             = <<EOF
+  # basic auth kubeconfig method will be removed in future release
+  # we will moving to token based auth method
+  kubeconfig   = <<EOF
 apiVersion: v1
 clusters:
 - cluster:
@@ -42,7 +44,7 @@ users:
     username: "${google_container_cluster.primary.master_auth[0].username}"
 
   EOF
-  bastion_name           = "${var.deployment_id}-bastion"
+  bastion_name = "${var.deployment_id}-bastion"
   # the second ternary is due to a bug during terraform destroy that the random_string.postgres_airflow_password
   # is an empty array and causes an error.  this just checks and lets it keep going through destroy successfully.
   postgres_airflow_password = (
