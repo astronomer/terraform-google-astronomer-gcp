8f747264fbc5bfe977e11db60349cc6604b36e48
pgvishnuram <vishnu@astronomer.io>
2021-09-29 15:06:26 +0530

    added output extras to utilize with token auth

--- locals.tf
+++ locals.tf
@@ -13,11 +13,14 @@ data "google_compute_instance" "sample_instance" {
 }
 
 locals {
-  project      = data.google_compute_zones.available.project
-  region       = data.google_compute_zones.available.region
-  zone         = data.google_compute_zones.available.names[0]
-  location     = var.zonal_cluster ? local.zone : local.region
-  kubeconfig   = <<EOF
+  project                = data.google_compute_zones.available.project
+  region                 = data.google_compute_zones.available.region
+  zone                   = data.google_compute_zones.available.names[0]
+  location               = var.zonal_cluster ? local.zone : local.region
+  cluster_name           = google_container_cluster.primary.name
+  endpoint               = google_container_cluster.primary.endpoint
+  cluster_ca_certificate = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
+  kubeconfig             = <<EOF
 apiVersion: v1
 clusters:
 - cluster:
@@ -39,7 +42,7 @@ users:
     username: "${google_container_cluster.primary.master_auth[0].username}"
 
   EOF
-  bastion_name = "${var.deployment_id}-bastion"
+  bastion_name           = "${var.deployment_id}-bastion"
   # the second ternary is due to a bug during terraform destroy that the random_string.postgres_airflow_password
   # is an empty array and causes an error.  this just checks and lets it keep going through destroy successfully.
   postgres_airflow_password = (
