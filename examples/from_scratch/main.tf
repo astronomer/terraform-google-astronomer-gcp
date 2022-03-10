3b030975f9bf16d252ea204fcd7f3391bf6fc5d0
pgvishnuram <81585115+pgvishnuram@users.noreply.github.com>
2022-03-10 22:22:13 +0530

    minor updates to gke terraform (#62)
    * updated gke default version
    * added dev dns zone
    * updates to email address

--- examples/from_scratch/main.tf
+++ examples/from_scratch/main.tf
@@ -11,8 +11,8 @@ variable "spotinist_token" {
 module "astronomer_gcp" {
   source                     = "../.."
   deployment_id              = var.deployment_id
-  dns_managed_zone           = "steven-zone"
-  email                      = "steven@astronomer.io"
+  dns_managed_zone           = "astrodev"
+  email                      = "infrastructure@astronomer.io"
   zonal_cluster              = var.zonal
   management_endpoint        = "public"
   enable_gke_metered_billing = true
