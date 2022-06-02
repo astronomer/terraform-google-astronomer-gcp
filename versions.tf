e2fda7b8e67f1bf0335e0fe3ad5e8fe34d042f9f
Mishal Shah <mishal.shah@astronomer.io>
2022-06-02 14:42:12 +0530

    Updating provider and Terraform version

--- versions.tf
+++ versions.tf
@@ -1,40 +1,42 @@
 
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.1.9"
   required_providers {
     acme = {
       source  = "vancluever/acme"
-      version = "~> 2.0"
+      version = "2.9.0"
     }
     google = {
       source = "hashicorp/google"
+      version = "4.23.0"
     }
     google-beta = {
       source = "hashicorp/google-beta"
+      version = "4.23.0"
     }
     http = {
       source  = "hashicorp/http"
-      version = "~> 1.1"
+      version = "2.1.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 1.3"
+      version = "2.2.3"
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 2.1"
+      version = "3.1.1"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 2.2"
+      version = "3.2.0"
     }
     spotinst = {
       source  = "spotinst/spotinst"
-      version = "~> 1.17"
+      version = "1.76.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 2.1"
+      version = "3.4.0"
     }
   }
 }
