722be4601fa027abea379d04c331b349691d8e89
Mishal Shah <mishal.shah@astronomer.io>
2022-06-03 12:15:50 +0530

    Revert "Updating provider and Terraform version"
    This reverts commit e2fda7b8e67f1bf0335e0fe3ad5e8fe34d042f9f.

--- versions.tf
+++ versions.tf
@@ -1,42 +1,40 @@
 
 terraform {
-  required_version = ">= 1.1.9"
+  required_version = ">= 0.13"
   required_providers {
     acme = {
       source  = "vancluever/acme"
-      version = "2.9.0"
+      version = "~> 2.0"
     }
     google = {
       source = "hashicorp/google"
-      version = "4.23.0"
     }
     google-beta = {
       source = "hashicorp/google-beta"
-      version = "4.23.0"
     }
     http = {
       source  = "hashicorp/http"
-      version = "2.1.0"
+      version = "~> 1.1"
     }
     local = {
       source  = "hashicorp/local"
-      version = "2.2.3"
+      version = "~> 1.3"
     }
     null = {
       source  = "hashicorp/null"
-      version = "3.1.1"
+      version = "~> 2.1"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.2.0"
+      version = "~> 2.2"
     }
     spotinst = {
       source  = "spotinst/spotinst"
-      version = "1.76.0"
+      version = "~> 1.17"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "3.4.0"
+      version = "~> 2.1"
     }
   }
 }
