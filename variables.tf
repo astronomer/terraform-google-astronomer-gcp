6487b0e3e02a1f97396a9be84bf56c8cc11672f4
Mishal Shah <mishal.shah@astronomer.io>
2022-07-21 12:51:50 +0530

    Updating cluster version

--- variables.tf
+++ variables.tf
@@ -9,7 +9,7 @@ variable "dns_managed_zone" {
 }
 
 variable "kube_version_gke" {
-  default     = "1.21.9-gke.1001"
+  default     = "1.21.12-gke.2200"
   description = "The kubernetes version to use in GKE"
 }
 
