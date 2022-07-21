d0d32ca141dad872485fd4e80bc36513004b6ab7
Mishal Shah <mishal.shah@astronomer.io>
2022-07-21 11:28:27 +0530

    Removing provider version declaration.

--- versions.tf
+++ versions.tf
@@ -4,7 +4,6 @@ terraform {
   required_providers {
     acme = {
       source  = "vancluever/acme"
-      version = "~> 2.0"
     }
     google = {
       source = "hashicorp/google"
@@ -14,27 +13,21 @@ terraform {
     }
     http = {
       source  = "hashicorp/http"
-      version = "~> 1.1"
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 1.3"
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 2.1"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 2.2"
     }
     spotinst = {
       source  = "spotinst/spotinst"
-      version = "~> 1.17"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 2.1"
     }
   }
 }
