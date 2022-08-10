e746344dbfa9c48d683f624dea390382a2c7c3b8
Mishal Shah <mishal.shah@astronomer.io>
2022-08-10 13:14:27 +0530

    Enabling dynamic port allocation.

--- network.tf
+++ network.tf
@@ -34,7 +34,8 @@ resource "google_compute_router" "router" {
   network = google_compute_network.core.self_link
 
   bgp {
-    asn = 64514
+    keepalive_interval = 0
+    asn                = 64514
   }
 
 }
@@ -51,12 +52,19 @@ resource "google_compute_router_nat" "nat" {
 
   name                               = "${var.deployment_id}-gke-${formatdate("MM-DD-hh-mm", timestamp())}"
   region                             = local.region
-  min_ports_per_vm                   = 128
+  max_ports_per_vm                   = 65536
+  min_ports_per_vm                   = 1024
+  enable_dynamic_port_allocation     = true
   router                             = google_compute_router.router.name
   nat_ip_allocate_option             = "MANUAL_ONLY"
   nat_ips                            = length(var.natgateway_external_ip_list) == 0 ? google_compute_address.address.*.self_link : var.natgateway_external_ip_list
   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
   tcp_established_idle_timeout_sec   = 7200
+
+  log_config {
+    enable = false
+    filter = "ALL"
+  }
 
   lifecycle {
     ignore_changes = [name]
