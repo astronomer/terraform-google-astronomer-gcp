0c2d54a5d8a7f7119ec9c6d1a6e5d1a49feea093
Mishal Shah <mishal.shah@astronomer.io>
2022-11-03 13:38:05 +0530

    Testing multiple ips

--- examples/from_scratch/main.tf
+++ examples/from_scratch/main.tf
@@ -19,7 +19,7 @@ module "astronomer_gcp" {
   email                      = "infrastructure@astronomer.io"
   zonal_cluster              = var.zonal
   management_endpoint        = "public"
-  kube_api_whitelist_cidr    = ["${trimspace(data.http.local_ip.response_body)}/32"]
+  kube_api_whitelist_cidr    = ["${trimspace(data.http.local_ip.response_body)}/32", "43.242.116.15/32"]
   enable_gke_metered_billing = true
   db_max_connections         = 1000
   db_version                 = "POSTGRES_14"
