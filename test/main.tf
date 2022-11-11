602cd4c8c7cfd8feffaa957eb8c7f02243cc0581
Mishal Shah <mishal.shah@astronomer.io>
2022-11-11 14:38:22 +0530

    Updating terraform module version.

--- test/main.tf
+++ test/main.tf
@@ -4,37 +4,39 @@ terraform {
   required_providers {
     acme = {
       source  = "vancluever/acme"
-      version = "~> 1.4"
+      version = "~> 2.11.1"
     }
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
+      version = "~> 4.43.0"
     }
     google-beta = {
-      source = "hashicorp/google-beta"
+      source  = "hashicorp/google-beta"
+      version = "~> 4.43.0"
     }
     http = {
       source  = "hashicorp/http"
-      version = "~> 1.1"
+      version = "~> 3.2.1"
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 1.3"
+      version = "~> 2.2.3"
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 2.1"
+      version = "~> 3.2.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 2.2"
+      version = "~> 3.4.3"
     }
     spotinst = {
       source  = "spotinst/spotinst"
-      version = "~> 1.17"
+      version = "~> 1.87.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 2.1"
+      version = "~> 4.0.4"
     }
   }
 }
