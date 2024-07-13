402acb2a586b31f99ef11f699549230d79d1e2ba
pgvishnuram <vishnu@astronomer.io>
2024-07-14 01:44:36 +0530

    update terraform version

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,7 +9,7 @@ variable "dns_managed_zone" {
 }
 
 variable "kube_version_gke" {
-  default     = "1.27.3-gke.100"
+  default     = "1.28.9-gke.1289002"
   description = "The kubernetes version to use in GKE"
 }
 
