72365eb39846690de2426162bc4ca57bc2be3bf9
pgvishnuram <vishnu@astronomer.io>
2024-07-14 00:08:31 +0530

    update outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -106,7 +106,7 @@ output "gcp_project" {
 }
 
 output "gcp_velero_backups_bucket_name" {
-  value = google_storage_bucket.velero_k8s_backup.name
+  value = google_storage_bucket.velero_k8s_backup[count.index].name
 }
 
 output "gcp_velero_service_account_email" {
